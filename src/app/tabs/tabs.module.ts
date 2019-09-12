47fbd2a2f6c6ecf6261408e5329fcdb395871fda
Christophe Biwer <biwerc@gmail.com>
2019-09-12 09:50:05 +0200

    some tslint changes

--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -1,19 +1,23 @@
-import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {IonicModule} from "@ionic/angular";
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
-import { TabsPageRoutingModule } from './tabs.router.module';
+import {TabsPageRoutingModule} from "./tabs.router.module";
 
-import { TabsPage } from './tabs.page';
+import {TabsPage} from "./tabs.page";
+import {TranslateModule} from "@ngx-translate/core";
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    TabsPageRoutingModule
-  ],
-  declarations: [TabsPage]
+    imports: [
+        IonicModule,
+        CommonModule,
+        FormsModule,
+        TabsPageRoutingModule,
+        TranslateModule
+
+    ],
+    declarations: [TabsPage]
 })
-export class TabsPageModule {}
+export class TabsPageModule {
+}
