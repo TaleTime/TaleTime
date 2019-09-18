1b7cd119dbc4f9c5491ad4eebcff98bc3f72fdd8
FE97 <45855522+FE97@users.noreply.github.com>
2019-09-18 15:33:03 +0200

    Update logger.ts

--- src/providers/logger/logger.ts
+++ src/providers/logger/logger.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { isDevMode } from "@angular/core";
 
 @Injectable()
-export class LoggerProvider {
+export class LoggerService {
   constructor() {}
   /**
    * log
