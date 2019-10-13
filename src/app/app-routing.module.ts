85abd17e6ec9dbbbaff61af2a6fadeeeb22bb85e
Cedric <cedric.c.schreiner@gmail.com>
2019-10-13 16:14:27 +0200

    The lazy routing used an old syntax which i updated to the new one. More information about the lazy routing can be found in the officially Documentation of Angular: https://angular.io/guide/lazy-loading-ngmodules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,34 +1,76 @@
-import {NgModule} from "@angular/core";
-import {PreloadAllModules, RouterModule, Routes} from "@angular/router";
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
     path: "",
-    loadChildren: () => import("./tabs/tabs.module").then(m => m.TabsPageModule)
+    loadChildren: () => import("./tabs/tabs.module").then(module => module.TabsPageModule)
   },
-  {path: "available-stories", loadChildren: "./pages/available-stories/available-stories.module#AvailableStoriesPageModule"},
+  {
+    path: "available-stories",
+    loadChildren: () => import("./pages/available-stories/available-stories.module").then(module => module.AvailableStoriesPageModule)
+  },
   {
     path: "change-user-account-pin",
-    loadChildren: "./pages/change-user-account-pin/change-user-account-pin.module#ChangeUserAccountPinPageModule"
+    loadChildren: () => import("./pages/change-user-account-pin/change-user-account-pin.module").then(module => module.ChangeUserAccountPinPageModule)
   },
-  {path: "create-user-account", loadChildren: "./pages/create-user-account/create-user-account.module#CreateUserAccountPageModule"},
-  {path: "create-user-profile", loadChildren: "./pages/create-user-profile/create-user-profile.module#CreateUserProfilePageModule"},
-  {path: "credits", loadChildren: "./pages/credits/credits.module#CreditsPageModule"},
-  {path: "info", loadChildren: "./pages/info/info.module#InfoPageModule"},
-  {path: "legal-information", loadChildren: "./pages/legal-information/legal-information.module#LegalInformationPageModule"},
-  {path: "player", loadChildren: "./pages/player/player.module#PlayerPageModule"},
-  {path: "select-user-profile", loadChildren: "./pages/select-user-profile/select-user-profile.module#SelectUserProfilePageModule"},
-  {path: "settings", loadChildren: "./pages/settings/settings.module#SettingsPageModule"},
-  {path: "start", loadChildren: "./pages/start/start.module#StartPageModule"},
-  {path: "story-details", loadChildren: "./pages/story-details/story-details.module#StoryDetailsPageModule"},
-  {path: "story-menu", loadChildren: "./pages/story-menu/story-menu.module#StoryMenuPageModule"},
-  {path: "tabs", loadChildren: "./tabs/tabs.module#TabsPageModule"},
-  {path: "user-account", loadChildren: "./pages/user-account/user-account.module#UserAccountPageModule"},
+  {
+    path: "create-user-account",
+    loadChildren: () => import("./pages/create-user-account/create-user-account.module").then(module => module.CreateUserAccountPageModule)
+  },
+  {
+    path: "create-user-profile",
+    loadChildren: () => import("./pages/create-user-profile/create-user-profile.module").then(module => module.CreateUserProfilePageModule)
+  },
+  {
+    path: "credits",
+    loadChildren: () => import("./pages/credits/credits.module").then(module => module.CreditsPageModule)
+  },
+  {
+    path: "info",
+    loadChildren: () => import("./pages/info/info.module").then(module => module.InfoPageModule)
+  },
+  {
+    path: "legal-information",
+    loadChildren: () => import("./pages/legal-information/legal-information.module").then(module => module.LegalInformationPageModule)
+  },
+  {
+    path: "player",
+    loadChildren: () => import("./pages/player/player.module").then(module => module.PlayerPageModule)
+  },
+  {
+    path: "select-user-profile",
+    loadChildren: () => import("./pages/select-user-profile/select-user-profile.module").then(module => module.SelectUserProfilePageModule)
+  },
+  {
+    path: "settings",
+    loadChildren: () => import("./pages/settings/settings.module").then(module => module.SettingsPageModule)
+  },
+  {
+    path: "start",
+    loadChildren: () => import("./pages/start/start.module").then(module => module.StartPageModule)
+  },
+  {
+    path: "story-details",
+    loadChildren: () => import("./pages/story-details/story-details.module").then(module => module.StoryDetailsPageModule)
+  },
+  {
+    path: "story-menu",
+    loadChildren: () => import("./pages/story-menu/story-menu.module").then(module => module.StoryMenuPageModule)
+  },
+  {
+    path: "tabs",
+    loadChildren: () => import("./tabs/tabs.module").then(module => module.TabsPageModule)
+  },
+  {
+    path: "user-account",
+    loadChildren: () => import("./pages/user-account/user-account.module").then(module => module.UserAccountPageModule)
+  }
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
