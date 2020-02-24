91b7f47e55d67e4c2b500cb41867af904ee8475e
xOnur <Onur_93@yahoo.de>
2020-02-24 16:05:08 +0100

    Gherkin + Cucumber + Chai + Example to use Test scenarios in /e2e folder. Run with -ng e2e- .

---
+++ e2e/src/test/steps/app.steps.ts
@@ -0,0 +1,24 @@
+import { Before, Given, Then, When } from 'cucumber';
+import { AppPage } from '../../pages/app.po';
+
+
+
+
+  Given('die URL {string}.', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+
+
+  When('die URL im Browser eingetragen wird,', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+
+
+  Then('soll die TaleTime Startpage erscheinen.', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
