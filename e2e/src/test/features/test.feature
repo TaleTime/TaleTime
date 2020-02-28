23d680ed9d365f81bd4e76a8c0d1653ca49b1a25
ChristianWi <christian-wirth@mailbox.org>
2020-02-28 18:25:45 +0100

    Implement Registartion-Button Test.

--- e2e/src/test/features/test.feature
+++ e2e/src/test/features/test.feature
@@ -1,9 +1,14 @@
 # language: en
 
-  Feature: These scenarios are used to check the functionality from a business perspective.
+  Feature: Checking navigation features
 
   Scenario: This scenario is used to check the availability of the Web site.
     Given is the URL "http://localhost:8100".
     When the URL is entered in the browser,
     Then the TaleTime startpage should appear.
+
+  Scenario: This scenario checks if the user can navigate from the start page to the Registration-Form
+    Given the start page "http://localhost:8100" is opened
+    When the user clicks on Registration-Button
+    Then the Registration-Form opens
 
--- e2e/src/test/steps/app.steps.ts
+++ e2e/src/test/steps/app.steps.ts
@@ -1,4 +1,5 @@
 import {Given, Status, Then, When} from 'cucumber';
+import {ExpectedConditions} from "protractor";
 
 const { Builder, By, Capabilities, Key } = require('selenium-webdriver');
 const { expect } = require('chai');
@@ -15,16 +16,31 @@ Given('is the URL {string}.', async function (string) {
   await driver.get(string);
 });
 
-
 When('the URL is entered in the browser,', async function () {
   // Write code here that turns the phrase above into concrete actions
 });
 
-
-
 Then('the TaleTime startpage should appear.', async function () {
   // Write code here that turns the phrase above into concrete actions
   if(await driver.findElement(By.id("startPageHeading")).isDisplayed()){
+    return Status.PASSED;
+  }
+  return Status.FAILED;
+});
+
+
+
+Given('the start page {string} is opened', function (string) {
+  driver.get(string);
+});
+
+When('the user clicks on Registration-Button', function () {
+  driver.findElements(By.id("registrationButton")).click();
+  return Status.PASSED;
+});
+
+Then('the Registration-Form opens', function () {
+  if(driver.findElement(By.id("registrationPageHeading")).isDisplayed()){
     return Status.PASSED;
   }
   return Status.FAILED;
