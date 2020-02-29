07cb3c9cbb9580b93d28f54c8bf908989b272f16
ChristianWi <christian-wirth@mailbox.org>
2020-02-29 19:32:19 +0100

    Refactored SettingsPage: changeTTS() and changeFontSize()

--- src/app/pages/settings/settings.page.spec.ts
+++ src/app/pages/settings/settings.page.spec.ts
@@ -10,6 +10,21 @@ import {SettingsService} from "../../services/settings/settings.service";
 import {TranslateService} from "@ngx-translate/core";
 import {AuthService} from "../../services/auth/auth.service";
 import {SimpleToastService} from "../../services/simple-toast/simple-toast.service";
+import {
+  FONT_SIZE_12_LABEL,
+  FONT_SIZE_12_VALUE,
+  FONT_SIZE_14_LABEL,
+  FONT_SIZE_14_VALUE, FONT_SIZE_16_LABEL,
+  FONT_SIZE_16_VALUE,
+  FONT_SIZE_18_LABEL,
+  FONT_SIZE_18_VALUE,
+  TTS_RATE_FAST,
+  TTS_RATE_FAST_VALUE,
+  TTS_RATE_NORMAL,
+  TTS_RATE_NORMAL_VALUE,
+  TTS_RATE_SLOW,
+  TTS_RATE_SLOW_VALUE
+} from "../../constants/constants";
 
 // describe("SettingsPage", () => {
 //   let component: SettingsPage;
@@ -67,29 +82,6 @@ class MockedToastService{
 
 }
 
-describe('Get code from language', () =>{
-  it('Return the matching language-code, in this case de-DE', () => {
-    expect(SettingsPage.getCodeFromLanguage('Deutsch'))
-      .toEqual('de-DE');
-  });
-  it('Return the matching language-code, in this case en-EN', () => {
-    expect(SettingsPage.getCodeFromLanguage('English'))
-      .toEqual('en-US');
-  });
-})
-
-
-describe('Get language from code', () =>{
-  it('Return the matching language-name, in this case Deutsch', () => {
-    expect(SettingsPage.getLanguageFromCode('de-DE'))
-      .toEqual('Deutsch');
-  });
-  it('Return the matching language-name, in this case English', () => {
-    expect(SettingsPage.getLanguageFromCode('en-US'))
-      .toEqual('English');
-  });
-})
-
 TestBed.configureTestingModule({
   declarations: [
 
@@ -113,38 +105,157 @@ let translateService = TestBed.get(TranslateService);
 let authService = TestBed.get(AuthService);
 let toastService = TestBed.get(SimpleToastService);
 
-let settingsPage =  new SettingsPage(navController, storage, router, settingsService, translateService, authService, toastService);
 
+/*
+ * Get code from language
+ */
+describe('Get code from language', () =>{
+  it('Return the matching language-code, in this case de-DE', () => {
+    expect(SettingsPage.getCodeFromLanguage('Deutsch'))
+      .toEqual('de-DE')
+  });
+  it('Return the matching language-code, in this case en-EN', () => {
+    expect(SettingsPage.getCodeFromLanguage('English'))
+      .toEqual('en-US')
+  });
+  it('Return the matching language-code, in this case null', () => {
+    expect(SettingsPage.getCodeFromLanguage('False input'))
+      .toBeNull()
+  });
+})
+
+
+/*
+ * Get language from code
+ */
+describe('Get language from code', () =>{
+  it('Return the matching language-name, in this case Deutsch', () => {
+    expect(SettingsPage.getLanguageFromCode('de-DE'))
+      .toEqual('Deutsch')
+  });
+  it('Return the matching language-name, in this case English', () => {
+    expect(SettingsPage.getLanguageFromCode('en-US'))
+      .toEqual('English')
+  });
+  it('Return the matching language-code, in this case null', () => {
+    expect(SettingsPage.getCodeFromLanguage('False input'))
+      .toBeNull()
+  });
+})
+
+
+/*
+ * Change language
+ */
 describe('Change language', () =>{
+  let settingsPage
+
   beforeEach(() => {
-    settingsPage.changeLanguage('English')
+    settingsPage =  new SettingsPage(navController, storage, router, settingsService, translateService, authService,
+      toastService);
+    settingsPage.settings.language = null
+  });
+
+  afterEach(() => {
+    settingsPage = null
   });
 
   it('Changes the language to english', () => {
+    settingsPage.changeLanguage('English')
     expect(settingsPage.settings.language)
       .toEqual('en-US')
   })
-})
-
-describe('Change language', () =>{
-  beforeEach(() => {
-    settingsPage.changeLanguage('Deutsch')
-  });
-
   it('Changes the language to german', () => {
+    settingsPage.changeLanguage('Deutsch')
     expect(settingsPage.settings.language)
       .toEqual('de-DE')
+  })
+  it('Changes the language to unknown language', () => {
+    settingsPage.changeLanguage('False input')
+    expect(settingsPage.settings.language)
+      .toBeNull()
   })
 })
 
 
-describe('Change language to unknown language', () =>{
+/*
+ * Change TTS rate
+ */
+describe('Change TTS rate', () =>{
+  let settingsPage
+
   beforeEach(() => {
-    settingsPage.changeLanguage('False input')
+    settingsPage =  new SettingsPage(navController, storage, router, settingsService, translateService, authService,
+      toastService);
+    settingsPage.settings.ttsRate = null;
   });
 
-  it('Changes the language to unknown language', () => {
-    expect(settingsPage.settings.language)
-      .toEqual(null)
+  afterEach(() => {
+    settingsPage = null
+  });
+
+  it('Changes TTS rate to slow', () => {
+    settingsPage.changeTtsRate(TTS_RATE_SLOW)
+    expect(settingsPage.settings.ttsRate)
+      .toEqual(TTS_RATE_SLOW_VALUE)
+  })
+  it('Changes TTS rate to normal', () => {
+    settingsPage.changeTtsRate(TTS_RATE_NORMAL)
+    expect(settingsPage.settings.ttsRate)
+      .toEqual(TTS_RATE_NORMAL_VALUE)
+  })
+  it('Changes TTS rate to fast', () => {
+    settingsPage.changeTtsRate(TTS_RATE_FAST)
+    expect(settingsPage.settings.ttsRate)
+      .toEqual(TTS_RATE_FAST_VALUE)
+  })
+  it('Changes TTS rate to unknown TTS', () => {
+      settingsPage.changeTtsRate('False input')
+      expect(settingsPage.settings.ttsRate)
+        .toBeNull()
+  })
+})
+
+
+/*
+ * Change Font size
+ */
+describe('Change Font size', () =>{
+  let settingsPage
+
+  beforeEach(() => {
+    settingsPage =  new SettingsPage(navController, storage, router, settingsService, translateService, authService,
+      toastService);
+    settingsPage.settings.fontSize = null;
+  });
+
+  afterEach(() => {
+    settingsPage = null
+  });
+
+  it('Changes font size to 12', () => {
+    settingsPage.changeFontSize(FONT_SIZE_12_LABEL)
+    expect(settingsPage.settings.fontSize)
+      .toEqual(FONT_SIZE_12_VALUE)
+  })
+  it('Changes font size to 14', () => {
+    settingsPage.changeFontSize(FONT_SIZE_14_LABEL)
+    expect(settingsPage.settings.fontSize)
+      .toEqual(FONT_SIZE_14_VALUE)
+  })
+  it('Changes font size to 16', () => {
+    settingsPage.changeFontSize(FONT_SIZE_16_LABEL)
+    expect(settingsPage.settings.fontSize)
+      .toEqual(FONT_SIZE_16_VALUE)
+  })
+  it('Changes font size to 18', () => {
+    settingsPage.changeFontSize(FONT_SIZE_18_LABEL)
+    expect(settingsPage.settings.fontSize)
+      .toEqual(FONT_SIZE_18_VALUE)
+  })
+  it('Changes font size to unknown font size', () => {
+    settingsPage.changeFontSize('False input')
+    expect(settingsPage.settings.fontSize)
+      .toBeNull()
   })
 })
