f4f491c46608677e7b950b8edbd54f9c875fcb0c
xOnur <Onur_93@yahoo.de>
2020-02-29 15:38:40 +0100

    "click" issue in protractor solved

--- e2e/src/test/features/test.feature
+++ e2e/src/test/features/test.feature
@@ -3,12 +3,12 @@
   Feature: Checking navigation features
 
   Scenario: This scenario is used to check the availability of the Web site.
-    Given is the URL "http://localhost:4200".
+    Given is the URL "http://localhost:8100".
     When the URL is entered in the browser,
     Then the TaleTime startpage should appear.
 
   Scenario: This scenario checks if the user can navigate from the start page to the Registration-Form
-    Given the start page "http://localhost:4200" is opened
+    Given the start page "http://localhost:8100" is opened
     When the user clicks on Registration-Button
     Then the Registration-Form should open
 
--- e2e/src/test/steps/app.steps.ts
+++ e2e/src/test/steps/app.steps.ts
@@ -1,5 +1,5 @@
 import {Given, Status, Then, When} from 'cucumber';
-import {ExpectedConditions} from "protractor";
+import Keys = Chai.Keys;
 
 const { Builder, By, Capabilities, Key } = require('selenium-webdriver');
 const { expect } = require('chai');
@@ -10,14 +10,13 @@ const capabilities = Capabilities.chrome();
 capabilities.set('chromeOptions', { "w3c": false });
 const driver = new Builder().withCapabilities(capabilities).build();
 
-
-Given('is the URL {string}.', async function (string) {
+Given('is the URL {string}.', {timeout: 2 * 5000},async function (string) {
   // Write code here that turns the phrase above into concrete actions
   await driver.get(string);
 });
 
 When('the URL is entered in the browser,', async function () {
-  // Write code here that turns the phrase above into concrete actions
+  return Status.PASSED;
 });
 
 Then('the TaleTime startpage should appear.', async function () {
@@ -35,7 +34,8 @@ Given('the start page {string} is opened', function (string) {
 });
 
 When('the user clicks on Registration-Button', function () {
-  driver.findElements(By.id("registrationButton")).click();
+  driver.findElement(By.id("registrationButton")).click();
+
   return Status.PASSED;
 });
 
