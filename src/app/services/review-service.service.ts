c527ffa52a67f9a2e3ca92787a9f4b960a9e3f01
StolzAlexander <alexander.stolz97@gmail.com>
2021-12-29 13:35:02 +0100

    Change the function to BehaviorSubject to ensure that child elements can also access elements with Subscribe.

--- src/app/services/review-service.service.ts
+++ src/app/services/review-service.service.ts
@@ -1,12 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { BehaviorSubject} from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ReviewServiceService {
 
-  public storyID:string;
-  public storyTitle:string;
+  private storyIDSource = new BehaviorSubject<string>("default id");
+  currentStoryId = this.storyIDSource.asObservable();
+
+  private storyTitleSource = new BehaviorSubject<string>("default title");
+  currentStoryTitle = this.storyTitleSource.asObservable();
 
   constructor() { }
+
+  changeStoryId(storyId: string){
+    this.storyIDSource.next(storyId);
+  }
+
+  changeStoryTitle(storyTitle: string){
+    this.storyTitleSource.next(storyTitle);
+  }
 }
